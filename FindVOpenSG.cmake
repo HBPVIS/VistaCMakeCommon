f5e10688a26ae1b54cc68b6fcb6176d75ff37dd4
dr165799 <dr165799@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2011-03-21 10:01:56 +0000

    UPD: minor fixes
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@20669 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVOpenSG.cmake
+++ FindVOpenSG.cmake
@@ -13,18 +13,23 @@ if( NOT VOPENSG_FOUND )
 
 		if( UNIX )
 			set( OPENSG_LIBRARY_DIRS ${OPENSG_ROOT_DIR}/lib/opt )
+			set( OPENSG_LIBRARIES
+				OSGWindowGLUT
+				OSGSystem
+				OSGBase
+			)
 		elseif( WIN32 )
 			set( OPENSG_LIBRARY_DIRS ${OPENSG_ROOT_DIR}/lib )
+			set( OPENSG_LIBRARIES
+				optimized OSGWindowGLUT
+				optimized OSGSystem
+				optimized OSGBase
+				debug OSGWindowGLUTD
+				debug OSGSystemD
+				debug OSGBaseD
+			)
 		endif( UNIX )
-		set( OPENSG_INCLUDE_DIR ${OPENSG_ROOT_DIR}/include )
-		set( OPENSG_LIBRARIES
-			optimized OSGWindowGLUT
-			optimized OSGSystem
-			optimized OSGBase
-			debug OSGWindowGLUTD
-			debug OSGSystemD
-			debug OSGBaseD
-		)
+		set( OPENSG_INCLUDE_DIR ${OPENSG_ROOT_DIR}/include )		
 		set( OPENSG_DEFINTIONS -DOSG_WITH_GLUT -DOSG_WITH_GIF -DOSG_WITH_TIF -DOSG_WITH_JPG -DOSG_BUILD_DLL -D_OSG_HAVE_CONFIGURED_H_ )
 	endif( OPENSG_ROOT_DIR )
 	
