02318ef58fe5a0f7cd09559c11c3e1c58fea0d29
dr165799 <dr165799@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2011-05-13 07:39:54 +0000

    CHG: temporary rename
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@21270 33c38b33-09d2-4561-8d4d-1bfa2f58f885

---
+++ FindVTMP.cmake
@@ -0,0 +1,158 @@
+include( FindPackageHandleStandardArgs )
+include( VistaFindUtils )
+
+if( NOT VVTK_FOUND )
+
+	# VTK hides it config file quite well, so we have to search it explicitely
+	set( _SEARCH_PREFIXES )
+	if( EXISTS "$ENV{${_PACKAGE_NAME_UPPER}_ROOT}" )
+		list( APPEND _SEARCH_PREFIXES
+				"$ENV{${_PACKAGE_NAME_UPPER}_ROOT}/${VISTA_HWARCH}/lib/*"
+				"$ENV{${_PACKAGE_NAME_UPPER}_ROOT}/lib/*"
+		)
+	endif( EXISTS "$ENV{${_PACKAGE_NAME_UPPER}_ROOT}" )
+	if( EXISTS "$ENV{VRDEV}" )
+		list( APPEND _SEARCH_PREFIXES
+				"$ENV{VRDEV}/VTK*/${VISTA_HWARCH}/lib/*"
+				"$ENV{VRDEV}/VTK*/lib/*"
+				"$ENV{VRDEV}/VTK/*/${VISTA_HWARCH}/lib/*"
+				"$ENV{VRDEV}/VTK/*/lib/*"
+				"$ENV{VRDEV}/vtk*/${VISTA_HWARCH}/lib/*"
+				"$ENV{VRDEV}/vtk*/lib/*"
+				"$ENV{VRDEV}/vtk/*/${VISTA_HWARCH}/lib/*"
+				"$ENV{VRDEV}/vtk/*/lib/*"
+		)
+	endif( EXISTS "$ENV{VRDEV}" )
+		
+	foreach( _PATH ${_SEARCH_PREFIXES} )
+		file( GLOB _TMP_FILES "${_PATH}/VTKConfig.cmake" )
+		foreach( _FILE ${_TMP_FILES} )
+			file( TO_CMAKE_PATH ${_FILE} _FOUND_FILE )
+			string( REPLACE "/VTKConfig.cmake" "" _FOUND_PATH ${_FOUND_FILE} )
+			file( TO_CMAKE_PATH ${_FOUND_PATH} _FOUND_PATH )
+			list( APPEND VTK_CONFIG_DIRS ${_FOUND_PATH} )
+		endforeach( _FILE ${_TMP_FILES} )		
+	endforeach( _PATH ${_PREFIX_PATHES} )
+	if( VTK_CONFIG_DIRS )
+		list( REMOVE_DUPLICATES VTK_CONFIG_DIRS )
+	endif( VTK_CONFIG_DIRS )
+	
+	find_package( VTK ${VTK_VERSION} QUIET PATHS ${VTK_CONFIG_DIRS} )
+	
+	if( VTK_FOUND )
+		# a VTKConfig.vmake has been found and loaded
+	
+		# check if debug libraries are available
+		set( _TMP_VTK_DEBUG_LIB "_TMP_VTK_DEBUG_LIB-NOTFOUND" CACHE INTERNAL "" FORCE )
+		find_library( _TMP_VTK_DEBUG_LIB "vtkCommonD" PATH ${VTK_LIBRARY_DIRS} )		
+		if( _TMP_VTK_DEBUG_LIB )
+			set( _DEBUG_AVAILABLE TRUE )
+		else( _TMP_VTK_DEBUG_LIB )
+			set( _DEBUG_AVAILABLE TRUE )
+		endif( _TMP_VTK_DEBUG_LIB )
+		
+		if( _DEBUG_AVAILABLE )
+			set( VTK_LIBRARIES	optimized vtkCommon
+								optimized vtkDICOMParser
+								optimized vtkexoIIc
+								optimized vtkexpat
+								optimized vtkFiltering
+								optimized vtkfreetype
+								optimized vtkftgl
+								optimized vtkGenericFiltering
+								optimized vtkGeovis
+								optimized vtkGraphics
+								optimized vtkHybrid
+								optimized vtkImaging
+								optimized vtkInfovis
+								optimized vtkIO
+								optimized vtkjpeg
+								optimized vtklibxml2
+								optimized vtkNetCDF
+								optimized vtkpng
+								optimized vtkRendering
+								optimized vtksys
+								optimized vtktiff
+								optimized vtkViews
+								optimized vtkVolumeRendering
+								optimized vtksqlite
+								optimized vtkmetaio
+								optimized vtkverdict
+								optimized vtkproj4
+								optimized vtkWidgets
+								optimized vtkzlib									
+								debug vtkCommonD
+								debug vtkDICOMParserD
+								debug vtkexoIIcD
+								debug vtkexpatD
+								debug vtkFilteringD
+								debug vtkfreetypeD
+								debug vtkftglD
+								debug vtkGenericFilteringD
+								debug vtkGeovisD
+								debug vtkGraphicsD
+								debug vtkHybridD
+								debug vtkImagingD
+								debug vtkInfovisD
+								debug vtkIOD
+								debug vtkjpegD
+								debug vtklibxml2D
+								debug vtkNetCDFD
+								debug vtkpngD
+								debug vtkRenderingD
+								debug vtksysD
+								debug vtktiffD
+								debug vtkViewsD
+								debug vtkVolumeRenderingD
+								debug vtksqliteD
+								debug vtkmetaioD
+								debug vtkverdictD
+								debug vtkproj4D
+								debug vtkWidgetsD
+								debug vtkzlibD
+			)
+			
+		elseif( _DEBUG_AVAILABLE ) # no debug libraries available
+			set( VTK_LIBRARIES	vtkCommon
+								vtkDICOMParser
+								vtkexoIIc
+								vtkexpat
+								vtkFiltering
+								vtkfreetype
+								vtkftgl
+								vtkGenericFiltering
+								vtkGeovis
+								vtkGraphics
+								vtkHybrid
+								vtkImaging
+								vtkInfovis
+								vtkIO
+								vtkjpeg
+								vtklibxml2
+								vtkNetCDF
+								vtkpng
+								vtkRendering
+								vtksys
+								vtktiff
+								vtkViews
+								vtkVolumeRendering
+								vtksqlite
+								vtkmetaio
+								vtkverdict
+								vtkproj4
+								vtkWidgets
+								vtkzlib
+			)
+			
+		endif( _DEBUG_AVAILABLE )
+		
+		# VTK's dlls are in the VTK_RUNTIME_LIBRARY_DIRS, so we have to add this to the library dirs
+		# to find them
+		set( VTK_LIBRARY_DIRS ${VTK_LIBRARY_DIRS} ${VTK_RUNTIME_LIBRARY_DIRS} )		
+		set( VTK_ROOT_DIR ${VTK_INSTALL_PREFIX} )
+		
+	endif( VTK_FOUND )
+endif( NOT VVTK_FOUND )
+
+find_package_handle_standard_args( VVtk "Vtk could not be found" VTK_ROOT_DIR )
+set( VTK_FOUND ${VVTK_FOUND} )
