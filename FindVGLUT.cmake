3c33abe8eb5056be15389fefb0668ec91037b7c1
dr165799 <dr165799@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2011-08-01 07:35:42 +0000

    FIX: re-including now checks how glut was found prior (custom or default)
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@22747 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVGLUT.cmake
+++ FindVGLUT.cmake
@@ -6,58 +6,69 @@ include( VistaFindUtils )
 
 if( NOT VGLUT_FOUND )
 
-	vista_find_package_root( GLUT "include/GL/freeglut.h" NAMES freeglut glut OpenSG )
-
-	find_path( GLUT_ROOT_DIR include/GL/freeglut.h
-			PATHS 	${GLUT_ROOT_DIR}
-					$ENV{FREEGLUT_ROOT}/${VISTA_HWARCH} $ENV{FREEGLUT_ROOT}
-					$ENV{GLUT_ROOT} $ENV{GLUT_ROOT}/${VISTA_HWARCH}
-					$ENV{OPENSG_ROOT} $ENV{OPENSG_ROOT}/${VISTA_HWARCH}
-					$ENV{VRDEV}/freeglut/${VISTA_HWARCH}  $ENV{VRDEV}/freeglut
-					$ENV{VRDEV}/glut/${VISTA_HWARCH}  $ENV{VRDEV}/glut
-					$ENV{VRDEV}/OpenSG/${VISTA_HWARCH}  $ENV{VRDEV}/OpenSG
-			CACHE "Glut/Freeglut package directory" )
-
-	find_library( GLUT_LIBRARIES NAMES freeglut freeglut-msvc90x86 glut glut32
-					PATHS ${GLUT_ROOT_DIR}/lib ${GLUT_ROOT_DIR}/lib/opt
-					NO_DEFAULT_PATH
-					CACHE "Glut/freeglut library" )
-
-	if( GLUT_ROOT_DIR AND GLUT_LIBRARIES )
-
-		set( GLUT_INCLUDE_DIRS ${GLUT_ROOT_DIR}/include )
-		set( GLUT_DEFINITIONS "" )
-		get_filename_component( GLUT_LIBRARY_DIRS ${GLUT_LIBRARIES} PATH )
-
-	else( GLUT_ROOT_DIR AND GLUT_LIBRARIES )
-
-		find_path( GLUT_ROOT_DIR include/GL/glut.h
-				PATHS	${GLUT_ROOT_DIR}
-						$ENV{GLUT_ROOT}/${VISTA_HWARCH} $ENV{GLUT_ROOT}
-						$ENV{VRDEV}/glut/${VISTA_HWARCH} $ENV{VRDEV}/glut
-				CACHE "Glut/Freeglut package directory" )
-		find_library( GLUT_LIBRARIES NAMES glut glut32 PATHS ${GLUT_ROOT_DIR}/lib ${GLUT_ROOT_DIR}/lib/opt )
+	if( NOT GLUT_ROOT_DIR )
+		# Glut was not found yet in prior config runs
+		# first, check if we find the root dir on our own
+		
+		vista_find_package_root( GLUT "include/GL/glut.h" NAMES freeglut glut OpenSG )
+		
+		find_library( GLUT_LIBRARIES NAMES freeglut freeglut-msvc90x86 glut glut32
+						PATHS "${GLUT_ROOT_DIR}/lib" "${GLUT_ROOT_DIR}/lib/opt"
+						NO_DEFAULT_PATH
+						CACHE "Glut/freeglut library" )
 
 		if( GLUT_ROOT_DIR AND GLUT_LIBRARIES )
 
-			set( GLUT_INCLUDE_DIRS ${GLUT_ROOT_DIR}/include )
-			set( GLUT_DEFINITIONS "-DUSE_NATIVE_GLUT" )
+			set( GLUT_INCLUDE_DIRS "${GLUT_ROOT_DIR}/include" )
+			get_filename_component( GLUT_LIBRARY_DIRS ${GLUT_LIBRARIES} PATH )
 
 		else( GLUT_ROOT_DIR AND GLUT_LIBRARIES )
+			
 			find_package( GLUT )
 
 			if( GLUT_FOUND )
-
-				set( GLUT_INCLUDE_DIRS ${GLUT_INCLUDE_DIR} )
-				set( GLUT_LIBRARIES ${GLUT_glut_LIBRARY} )
-				set( GLUT_DEFINITIONS "-DUSE_NATIVE_GLUT" )
-				get_filename_component( GLUT_LIBRARY_DIRS ${GLUT_LIBRARIES} PATH )
-
+				set( GLUT_INCLUDE_DIRS "${GLUT_INCLUDE_DIR}" )
+				set( GLUT_ROOT_DIR "${GLUT_INCLUDE_DIRS}" )
+				set( GLUT_LIBRARIES "${GLUT_glut_LIBRARY}" )
+				get_filename_component( GLUT_LIBRARY_DIRS "${GLUT_LIBRARIES}" PATH )
+				set( GLUT_FOUND_BY_DEFAULT_MODULE TRUE CACHE INTERNAL "" FORCE )
 			endif( GLUT_FOUND )
 
 		endif( GLUT_ROOT_DIR AND GLUT_LIBRARIES )
+		
+	else()
+		# we already found glut on prior config run -> GLUT_ROOT_DIR is set
+		# we now have to differentiate between custom found, or with native find_glut
+		if( NOT GLUT_FOUND_BY_DEFAULT_MODULE )
+			find_library( GLUT_LIBRARIES NAMES freeglut freeglut-msvc90x86 glut glut32
+						PATHS "${GLUT_ROOT_DIR}/lib" "${GLUT_ROOT_DIR}/lib/opt"
+						NO_DEFAULT_PATH
+						CACHE "Glut/freeglut library" )
+						
+			set( GLUT_INCLUDE_DIRS "${GLUT_ROOT_DIR}/include" )
+			get_filename_component( GLUT_LIBRARY_DIRS "${GLUT_LIBRARIES}" PATH )
+			
+		else( NOT GLUT_FOUND_BY_DEFAULT_MODULE )
+		
+			find_package( GLUT )
 
-	endif( GLUT_ROOT_DIR AND GLUT_LIBRARIES )
+			if( GLUT_FOUND )
+				set( GLUT_INCLUDE_DIRS "${GLUT_INCLUDE_DIR}" )
+				set( GLUT_ROOT_DIR "${GLUT_INCLUDE_DIRS}" )
+				set( GLUT_LIBRARIES "${GLUT_glut_LIBRARY}" )
+				get_filename_component( GLUT_LIBRARY_DIRS "${GLUT_LIBRARIES}" PATH )
+				set( GLUT_FOUND_BY_DEFAULT_MODULE TRUE CACHE INTERNAL "" FORCE )
+			endif( GLUT_FOUND )
+		endif( NOT GLUT_FOUND_BY_DEFAULT_MODULE )
+			
+	endif( NOT GLUT_ROOT_DIR )
+	
+	# check if we do have a real glut
+	if( NOT EXISTS "${GLUT_INCLUDE_DIRS}/GL/freeglut.h" )
+		set( GLUT_DEFINITIONS "-DUSE_NATIVE_GLUT" )
+	else()
+		set( GLUT_DEFINITIONS "" )
+	endif( NOT EXISTS "${GLUT_INCLUDE_DIRS}/GL/freeglut.h" )
 
 endif( NOT VGLUT_FOUND )
 
