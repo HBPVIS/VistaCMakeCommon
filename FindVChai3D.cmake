d1803d738faaa4e5ff8ee80e37fc553f0963a445
ingoassenmacher <ingoassenmacher@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2011-09-01 11:22:17 +0000

    FIX: linux build chai3d
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@23127 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVChai3D.cmake
+++ FindVChai3D.cmake
@@ -11,8 +11,18 @@ if( NOT VCHAI3D_FOUND )
 		# Set platform specific CHAI3D path here
 		if( UNIX )
 			#UNIX is easy, as usual
-			set( CHAI3D_LIBRARY_DIRS ${CHAI3D_ROOT_DIR}/bin ${CHAI3D_ROOT_DIR}/lib/lin-i686  )
+			set( CHAI3D_LIBRARY_DIRS ${CHAI3D_ROOT_DIR}/bin ${CHAI3D_ROOT_DIR}/lib/lin-x86_64  )
 			message( WARNING "FindPackageCHAI3D - UNIX/Linux configuration is untested" )
+			if( ${CMAKE_SYSTEM_NAME} MATCHES "Linux" )
+			    set( CHAI3D_DEFINITIONS -D_LINUX )
+			endif( ${CMAKE_SYSTEM_NAME} MATCHES "Linux" )
+		
+		set( CHAI3D_LIBRARIES
+			optimized chai3d
+			debug chai3d-dbg
+		)
+			
+			
 		elseif(WIN32)
 			#For WIN32, CHAI3D contains specific libraries for different compilers
 			if( MSVC )
@@ -23,19 +33,21 @@ if( NOT VCHAI3D_FOUND )
 				else( MSVC80 )
 					message( WARNING "FindPackageCHAI3D - Unknown MSVC version" )
 				endif( MSVC80 )
+				set( CHAI3D_DEFINITIONS -D_MSVC )
 			else( MSVC )
 				message( WARNING "FindPackageCHAI3D - using WIN32 without Visual Studio - this will probably fail - use at your own risk!" )
 			endif( MSVC )	
-		endif(UNIX)
-		set( CHAI3D_LIBRARIES
-			optimized chai3d-release
-			debug chai3d-debug
+			
+     		set( CHAI3D_LIBRARIES
+	    		optimized chai3d-release
+		    	debug chai3d-debug
 		)
-		set( CHAI3D_DEFINITIONS -D_MSVC )
+		endif(UNIX)
+		
 	else( CHAI3D_ROOT_DIR )
 		message( WARNING "vista_find_package_root - scr/chai3d.h not found" )	
 	endif( CHAI3D_ROOT_DIR )
 endif( NOT VCHAI3D_FOUND )
 
-find_package_handle_standard_args( VChai3d "Chai3D could not be found" CHAI3D_ROOT_DIR CHAI3D_LIBRARIES )
+find_package_handle_standard_args( VChai3D "Chai3D could not be found" CHAI3D_ROOT_DIR CHAI3D_LIBRARIES )
 
