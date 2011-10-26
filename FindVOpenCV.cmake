6d4d1f0a293a16d21c3506f7cc2a0fc493fac3be
dr165799 <dr165799@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2011-10-26 11:30:31 +0000

    FIX: FindOpenCV checking linux-style distribution
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@23881 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVOpenCV.cmake
+++ FindVOpenCV.cmake
@@ -6,18 +6,20 @@ include( VistaFindUtils )
 if( NOT VOPENCV_FOUND )
 
 	# search config file
-	vista_find_package_root( OpenCV OpenCVConfig.cmake )
+	vista_find_package_root( OpenCV OpenCVConfig.cmake NAMES OpenCV ADVANCED )
+	if( NOT OPENCV_ROOT_DIR )
+		vista_find_package_root( OpenCV share/OpenCV/OpenCVConfig.cmake ADVANCED )
+	endif()
 
-	if( OPENCV_ROOT_DIR )
-		set( OPENCV_FOUND )
-		find_package( OpenCV QUIET PATHS ${OPENCV_ROOT_DIR} NO_DEFAULT_PATH )
-		# an OpenCVConfig.cmake has been found and loaded
-		
+	set( OPENCV_FOUND )
+	find_package( OpenCV QUIET PATHS ${OPENCV_ROOT_DIR} NO_DEFAULT_PATH )
+	# an OpenCVConfig.cmake has been found and loaded
+	if( OPENCV_FOUND )
 		set( OPENCV_LIBRARIES "${OpenCV_LIBS}" )
 		set( OPENCV_LIBRARY_DIRS "${OpenCV_LIB_DIR}" "${OpenCV_DIR}/bin"  )
 		set( OPENCV_INCLUDE_DIRS "${OpenCV_INCLUDE_DIRS}" )
 		set( OPENCV_VERSION "${OpenCV_VERSION}" )
-	endif( OPENCV_ROOT_DIR )
+	endif()
 	
 endif( NOT VOPENCV_FOUND )
 
