79ff023cdaf3f5cf860eb591c55bf09bf7df8cb3
tk674006 <tk674006@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2011-11-07 18:23:49 +0000

    ADD: Preliminary Find for XFEM Solver developed by the CATS in context of the BSSO project
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@24079 33c38b33-09d2-4561-8d4d-1bfa2f58f885

---
+++ FindVCatsXFEMSolver.cmake
@@ -0,0 +1,19 @@
+# $Id: FindVEigen.cmake 21495 2011-05-25 07:52:18Z dr165799 $
+
+include( FindPackageHandleStandardArgs )
+include( VistaFindUtils )
+
+if( NOT VCATSXFEMSOLVER_FOUND )
+	vista_find_package_root( CatsXFEMSolver CatsXFEMSolver/XFEM.h )	
+
+	if( CATSXFEMSOLVER_ROOT_DIR )
+		set( CATSXFEMSOLVER_INCLUDE_DIRS ${CATSXFEMSOLVER_ROOT_DIR})	
+		set( CATSXFEMSOLVER_LIBRARY_DIRS ${CATSXFEMSOLVER_ROOT_DIR})
+		set( CATSXFEMSOLVER_LIBRARIES libgoto2_nehalemp-r1.13 umfpack libamd libg2c libgcc libshared libSources)
+	else( CATSXFEMSOLVER_ROOT_DIR )
+		message( WARNING "vista_find_package_root - File named CatsXFEMSolver/XFEM.h not found" )	
+	endif( CATSXFEMSOLVER_ROOT_DIR )
+endif( NOT VCATSXFEMSOLVER_FOUND )
+
+find_package_handle_standard_args( VCATSXFEMSOLVER "CatsXFEMSolver could not be found" CATSXFEMSOLVER_ROOT_DIR )
+
