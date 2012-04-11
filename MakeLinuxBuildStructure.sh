cd5600d9ae5a12f6854b1dd31c2099c273640bf4
dr165799 <dr165799@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2012-04-11 13:59:43 +0000

    ADD: script for automatic build tree and scripts to configure+make debug and release simultanioulsy
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@29878 33c38b33-09d2-4561-8d4d-1bfa2f58f885

---
+++ MakeLinuxBuildStructure.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+function usage()
+{
+	echo "Usage:   MakeLinuxBuildStructure.sh [options]"
+	echo "   Creates cmake configs for both debug and release builds"
+	echo "   and creates a Makefile script to make / run cmake on both"
+	echo "   builds simultaniously"
+    echo "Options"
+	echo "    -h, --help"
+    echo "   ( cmake | ccmake | cmake-gui ) - specify the cmake binary to use"
+	echo "                                    when calling RerunCMake.sh"
+	echo "                                    default: ccmake"
+	echo "   -releasedir DIR                - name of the release subdirectory"
+	echo "                                    default: release"
+    echo "   -debugdir DIR                  - name of the debug subdirectory"
+	echo "                                    default: debug"
+	echo "   -topdir DIR                    - toplevel subdir for build structure"
+	echo "                                    default: debug"
+    echo ""
+}
+
+# GENERAL_BUILD_DIR may be empty, the others have to be filled
+GENERAL_BUILD_DIR=build_LINUX.X86_64
+DEBUG_BUILD_DIR=debug
+RELEASE_BUILD_DIR=release
+CMAKE_BINARY=ccmake
+
+while [ "$1" != "" ]; do
+	case $1 in
+        cmake-gui | cmake | ccmake )
+            CMAKE_BINARY=$1
+            ;;
+		-h | --help )
+            usage
+			exit
+            ;;
+        *)
+            echo Unexpected parameter $1
+			usage
+			exit
+            ;;
+    esac
+	shift
+done
+
+if [ ! "$GENERAL_BUILD_DIR" == "" ]; then
+	if [ ! -d $RELEASE_BUILD_DIR ]; then
+		mkdir $RELEASE_BUILD_DIR
+	fi
+	mkdir $GENERAL_BUILD_DIR
+	cd $GENERAL_BUILD_DIR
+fi
+
+if [ ! -d $RELEASE_BUILD_DIR ]; then
+	mkdir $RELEASE_BUILD_DIR
+fi
+if [ ! -d $DEBUG_BUILD_DIR ]; then
+	mkdir $DEBUG_BUILD_DIR
+fi
+
+cd $RELEASE_BUILD_DIR
+
+cmake -DCMAKE_BUILD_TYPE=Release ../..
+
+cd ..
+
+cp -R $RELEASE_BUILD_DIR/CMakeFiles $DEBUG_BUILD_DIR
+sed "s/\/$RELEASE_BUILD_DIR/\/$DEBUG_BUILD_DIR/g" $RELEASE_BUILD_DIR/CMakeCache.txt > $DEBUG_BUILD_DIR/CMakeCache.txt
+
+cd $DEBUG_BUILD_DIR
+
+
+cmake -DCMAKE_BUILD_TYPE=Debug ../..
+
+cd ..
+
+cp ${VISTA_CMAKE_COMMON}/LinuxBuildStructureMakefile .
+mv LinuxBuildStructureMakefile Makefile.tmp1
+sed "s/RELEASEDIR/$RELEASE_BUILD_DIR/g" Makefile.tmp1 > Makefile.tmp2
+sed "s/DEBUGDIR/$DEBUG_BUILD_DIR/g" Makefile.tmp2 > Makefile
+rm Makefile.tmp*
+
+cp ${VISTA_CMAKE_COMMON}/LinuxBuildStructureRerunCMake .
+mv LinuxBuildStructureRerunCMake RerunCMake.sh.tmp1
+sed "s/RELEASEDIR/$RELEASE_BUILD_DIR/g" RerunCMake.sh.tmp1 > RerunCMake.sh.tmp2
+sed "s/DEBUGDIR/$DEBUG_BUILD_DIR/g" RerunCMake.sh.tmp2 > RerunCMake.sh.tmp3
+sed "s/CMAKE_CONFIG_BINARY/$CMAKE_BINARY/g" RerunCMake.sh.tmp3 > RerunCMake.sh
+rm RerunCMake.sh.tmp*
+chmod ug+x RerunCMake.sh
