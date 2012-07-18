676771ab12076ebf53cbeec5d531cf1c4b184575
tk674006 <tk674006@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2012-07-18 08:38:00 +0000

    ADD: separated debug release setup
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@31205 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVCatsXFEMSolver.cmake
+++ FindVCatsXFEMSolver.cmake
@@ -12,7 +12,19 @@ if( NOT VCATSXFEMSOLVER_FOUND )
 		if ( CATSXFEMSOLVER_VERSION_STRING VERSION_LESS 1.2)
 			set( CATSXFEMSOLVER_LIBRARIES libgoto2_nehalemp-r1.13 umfpack libamd libg2c libgcc libshared libSources)
 		else ()
-			set( CATSXFEMSOLVER_LIBRARIES libgoto2_nehalemp-r1.13 umfpack libamd libg2c libgcc Propagation)
+			set( CATSXFEMSOLVER_LIBRARIES 
+				optimized libgoto2_nehalemp-r1.13
+				optimized umfpack
+				optimized libamd 
+				optimized libg2c 
+				optimized libgcc 
+				optimized Propagation
+				debug libgoto2_nehalemp-r1.13
+				debug umfpack
+				debug libamd 
+				debug libg2c 
+				debug libgcc 
+				debug PropagationD)
 		endif()
 	else( CATSXFEMSOLVER_ROOT_DIR )
 		message( WARNING "vista_find_package_root - File named CatsXFEMSolver/XFEM.h not found" )	
