8368106dc9c24ec37b599a921e1334cca01929c8
cd933740 <cd933740@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2012-09-04 11:11:37 +0000

    UPD: added support for debug and release libs
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@31901 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVOpenMesh.cmake
+++ FindVOpenMesh.cmake
@@ -8,15 +8,33 @@ if( NOT VOPENMESH_FOUND )
 	vista_find_package_root( OPENMESH include/OpenMesh/Core/Mesh/TriMeshT.hh )
 
 	if( OPENMESH_ROOT_DIR )
-		find_library( OPENMESH_LIBRARIES NAMES OpenMeshCore OpenMeshTools OPENMESH
-					PATHS ${OPENMESH_ROOT_DIR}/lib
-					CACHE "OpenMesh library" )
-		mark_as_advanced( OPENMESH_LIBRARIES )
-
-		set( OPENMESH_INCLUDE_DIRS ${OPENMESH_ROOT_DIR}/include )
-		set( OPENMESH_LIBRARY_DIRS ${OPENMESH_ROOT_DIR}/lib )
-		get_filename_component( OPENMESH_LIBRARY_DIRS ${OPENMESH_LIBRARIES} PATH )
-
+		set( OPENMESH_INCLUDE_DIRS "${OPENMESH_ROOT_DIR}/include" )
+		set( OPENMESH_LIBRARY_DIRS "${OPENMESH_ROOT_DIR}/lib" )
+		
+		vista_find_library_uncached_var( _RELEASE_LIB OpenMeshCore PATHS ${OPENMESH_LIBRARY_DIRS} NO_DEFAULT_PATH )
+		vista_find_library_uncached_var( _DEBUG_LIB OpenMeshCoreD PATHS ${OPENMESH_LIBRARY_DIRS} NO_DEFAULT_PATH )
+		if( _RELEASE_LIB AND _DEBUG_LIB )
+			set( OPENMESH_LIBRARIES
+				optimized OpenMeshCore
+				optimized OpenMeshTools
+				debug	  OpenMeshCoreD
+				debug	  OpenMeshToolsD
+			)
+		elseif( UNIX AND _RELEASE_LIB )
+			set( OPENMESH_LIBRARIES
+				OpenMeshCore
+				OpenMeshTools
+			)
+		elseif( NOT OPENMESH_FIND_QUIET )
+			if( NOT _RELEASE_LIB AND NOT _DEBUG_LIB )
+				message( WARNING "OpenMesh libraries could not be found in \"${OPENMESH_LIBRARY_DIRS}\"" )
+			elseif( NOT _DEBUG_LIB )
+				message( WARNING "OpenMesh debug libraries could not be found in \"${OPENMESH_LIBRARY_DIRS}\"" )
+			else()
+				message( WARNING "OpenMesh release libraries could not be found in \"${OPENMESH_LIBRARY_DIRS}\"" )
+			endif()
+		endif()
+		
 	endif( OPENMESH_ROOT_DIR )
 
 endif( NOT VOPENMESH_FOUND )
