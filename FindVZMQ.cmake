878906b4cd1af1f941546fae6409e026c60785a4
bh736275 <bh736275@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2013-05-27 14:12:26 +0000

    UPD: support for zmq 3.x
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@35237 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVZMQ.cmake
+++ FindVZMQ.cmake
@@ -5,17 +5,42 @@ include( VistaFindUtils )
 
 if( NOT VZMQ_FOUND )
 
-	vista_find_package_root( ZMQ include/zmq.h NAMES ZeroMQ zeromq Zeromq)
+	vista_find_package_root( ZMQ include/zmq.h NAMES ZMQ zeromq ZeroMQ Zeromq DEBUG_OUTPUT)
 
 	if( ZMQ_ROOT_DIR )
+		
 		vista_find_library_uncached( NAMES zmq ZMQ libzmq
 									PATHS "${ZMQ_ROOT_DIR}/lib"
 									CACHE "ZMQ library" )
 
 		set( ZMQ_INCLUDE_DIRS ${ZMQ_ROOT_DIR}/include )
-		set( ZMQ_LIBRARY_DIRS ${ZMQ_ROOT_DIR}/lib )
-		get_filename_component( ZMQ_LIBRARY_DIRS "${VISTA_UNCACHED_LIBRARY}" PATH )
-		get_filename_component( ZMQ_LIBRARIES "${VISTA_UNCACHED_LIBRARY}" NAME )
+		
+		if( ${ZMQ_VERSION_STRING} VERSION_GREATER 3.0)
+			if( UNIX )
+				#UNIX is easy, as usual
+				set( ZMQ_LIBRARY_DIRS ${ZMQ_ROOT_DIR}/lib )
+			elseif(WIN32)
+				#zmq 3.x puts the dlls in a separate bin directory AND differentiates 
+				#between win32 and x86_64 builds
+				if(VISTA_64BIT) 
+					set( ZMQ_LIBRARY_DIRS 
+						 ${ZMQ_ROOT_DIR}/lib/x64
+						 ${ZMQ_ROOT_DIR}/bin/x64 )
+				elseif(VISTA_32BIT)
+					set( ZMQ_LIBRARY_DIRS 
+						 ${ZMQ_ROOT_DIR}/lib/Win32
+						 ${ZMQ_ROOT_DIR}/bin/Win32 )
+				endif(VISTA_64BIT)
+				#zmq insists on libraries being named libzmq.lib and libzmq.dll
+				set(ZMQ_LIBRARIES libzmq)
+			endif(UNIX)
+		else( )
+			#for older versions ==> just use the standard lib path
+			set( ZMQ_LIBRARY_DIRS ${ZMQ_ROOT_DIR}/lib)
+		endif( )
+		
+		#get_filename_component( ZMQ_LIBRARY_DIRS "${VISTA_UNCACHED_LIBRARY}" PATH )
+		#get_filename_component( ZMQ_LIBRARIES "${VISTA_UNCACHED_LIBRARY}" NAME )
 	endif( ZMQ_ROOT_DIR )
 
 endif( NOT VZMQ_FOUND )
