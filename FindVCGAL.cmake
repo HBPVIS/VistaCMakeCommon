a1e7180e43eeabf7acb54b05b48952b8dab0fbcd
dr165799 <dr165799@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2013-10-29 12:14:44 +0000

    UPD: proper usage of found root dir for CGAL
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@38996 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVCGAL.cmake
+++ FindVCGAL.cmake
@@ -5,9 +5,12 @@ include( VistaFindUtils )
 
 if( NOT VCGAL_FOUND )
 	
-	vista_find_package_root( CGAL "include/CGAL/AABB_tree.h" )	
+	vista_find_package_root( CGAL "include/CGAL/AABB_tree.h" DEBUG_OUTPUT )	
+	if( CGAL_ROOT_DIR )
+		set( CGAL_DIR "${CGAL_ROOT_DIR}/lib/CGAL" )
+	endif()
 
-	vista_find_original_package( CGAL )
+	vista_find_original_package( VCGAL )
 	if( CGAL_FOUND )
 		file( TO_CMAKE_PATH "${CGAL_LIBRARIES_DIR}" _BIN_PATH )
 		set( CGAL_LIBRARY_DIRS ${CGAL_LIBRARY_DIRS} "${CGAL_LIBRARIES_DIR}/../bin" )
