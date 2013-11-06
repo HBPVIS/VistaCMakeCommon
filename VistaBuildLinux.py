832d0569d075af1763c3aa97b62a71ca5cdf9892
ah548455 <ah548455@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2013-11-06 09:43:31 +0000

    CHG making tests about test behaviour :)
    failed test shall not cause the jenkins build to fail. 
    
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@39127 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- VistaBuildLinux.py
+++ VistaBuildLinux.py
@@ -99,9 +99,9 @@ def MakeJenkinsBuild(strBuildType, strCompiler, strCMakeVariables, bDeleteCMakeC
     
     #execute tests
     if True == bRunTests:
-        iRC, strConsoleOutput = VistaPythonCommon.SysCall(strCompilerEnv + 'make test',ExitOnError = False)
-        if 0 != iRC:
-            iRC, strConsoleOutput = VistaPythonCommon.SysCall(strCompilerEnv + 'make test_verbose',ExitOnError = True)
+        iRC, strConsoleOutput = VistaPythonCommon.SysCall(strCompilerEnv + 'make test_outputonfailure',ExitOnError = False)
+        #if 0 != iRC:
+        #    iRC, strConsoleOutput = VistaPythonCommon.SysCall(strCompilerEnv + 'make test_verbose',ExitOnError = True)
         sys.stdout.write(strConsoleOutput)
         sys.stdout.flush()
         
