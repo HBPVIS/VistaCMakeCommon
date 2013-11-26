6ecae2089836f191abd8e5dd2460ed29a65ff7c6
dr165799 <dr165799@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2013-11-26 08:21:11 +0000

    STYLE: removed debug output
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@39493 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVCGAL.cmake
+++ FindVCGAL.cmake
@@ -5,16 +5,7 @@ include( VistaFindUtils )
 
 if( NOT VCGAL_FOUND )
 	
-	vista_find_package_root( CGAL "include/CGAL/AABB_tree.h" DEBUG_OUTPUT )	
-	
-	#if( CGAL_ROOT_DIR )
-	#	set( CGAL_DIR "${CGAL_ROOT_DIR}/lib/CGAL" )
-	#endif()
-	#vista_find_original_package( VCGAL )
-	#if( CGAL_FOUND )
-	#	file( TO_CMAKE_PATH "${CGAL_LIBRARIES_DIR}" _BIN_PATH )
-	#	set( CGAL_LIBRARY_DIRS ${CGAL_LIBRARY_DIRS} "${CGAL_LIBRARIES_DIR}/../bin" )
-	#endif()
+	vista_find_package_root( CGAL "include/CGAL/AABB_tree.h" )	
 	
 	if( CGAL_ROOT_DIR )
 		# @TODO: boost_all_dyn_link not nice here...
