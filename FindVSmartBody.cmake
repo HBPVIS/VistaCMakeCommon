5bf1908ee3e2a6d7ddae18c3bcca263b410cdea4
ah548455 <ah548455@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2014-01-05 21:41:59 +0000

    adopted to Smartbody SDK
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@39947 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVSmartBody.cmake
+++ FindVSmartBody.cmake
@@ -4,18 +4,25 @@ include( FindPackageHandleStandardArgs )
 include( VistaFindUtils )
 
 if( NOT VSMARTBODY_FOUND )
-	vista_find_package_root( SmartBody /core/smartbody/SmartBody/src/sb/SBTypes.h )
+	vista_find_package_root( SmartBody /src/SmartBody/sb/SBTypes.h )
 	if( SMARTBODY_ROOT_DIR )
 		set( SMARTBODY_INCLUDE_DIRS 
-            ${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/src/ 
-            ${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody-dll/include 
-            ${SMARTBODY_ROOT_DIR}/lib/vhcl/include
-            ${SMARTBODY_ROOT_DIR}/lib/boost/
-            ${SMARTBODY_ROOT_DIR}/lib/bonebus/include
-            ${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/steerlib/include
-            ${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/external/
-            ${SMARTBODY_ROOT_DIR}/lib/wsp/wsp/include/
-            ${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/src/external/glew/
+			${SMARTBODY_ROOT_DIR}/src/SmartBody
+			${SMARTBODY_ROOT_DIR}/include
+			${SMARTBODY_ROOT_DIR}/include/vhcl
+			${SMARTBODY_ROOT_DIR}/include/steersuite			
+			${SMARTBODY_ROOT_DIR}/python27/include
+			${SMARTBODY_ROOT_DIR}/include/bonebus
+			${SMARTBODY_ROOT_DIR}/include/wsp
+            #${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/src/ 
+            #${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody-dll/include 
+            #${SMARTBODY_ROOT_DIR}/lib/vhcl/include
+            #${SMARTBODY_ROOT_DIR}/lib/boost/
+            #${SMARTBODY_ROOT_DIR}/lib/bonebus/include
+            #${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/steerlib/include
+            #${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/external/
+            #${SMARTBODY_ROOT_DIR}/lib/wsp/wsp/include/
+            #${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/src/external/glew/
             
         )
         
@@ -31,47 +38,49 @@ if( NOT VSMARTBODY_FOUND )
         
         if(CMAKE_SIZEOF_VOID_P EQUAL 8)
             set( SMARTBODY_LIBRARY_DIRS 
-            ${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/lib
-            ${SMARTBODY_ROOT_DIR}/lib/vhcl/lib64
-            ${SMARTBODY_ROOT_DIR}/lib/boost/lib
-            ${SMARTBODY_ROOT_DIR}/lib/bonebus/lib
-            ${SMARTBODY_ROOT_DIR}/lib/wsp/wsp/lib
-            ${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/build/win32/Debug
-            ${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/build/win32/Release
-            ${SMARTBODY_ROOT_DIR}/lib/vhmsg/vhmsg-c/lib64
-            ${SMARTBODY_ROOT_DIR}/lib/activemq/activemq-cpp/vs2010-build/DebugDLL64
-            ${SMARTBODY_ROOT_DIR}/lib/activemq/activemq-cpp/vs2010-build/ReleaseDLL
-            ${SMARTBODY_ROOT_DIR}/lib/xerces-c/lib
-		/usr/local_rwth/sw/boost/1_53_0/gcc_4.4.6-openmpi_1.6.4/lib/
-		/usr/local_rwth/sw/python/2.7.5/x86_64/lib/
-            ${SMARTBODY_ROOT_DIR}/core/smartbody/Python27/libs
+            #${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/lib
+            #${SMARTBODY_ROOT_DIR}/lib/vhcl/lib64
+            #${SMARTBODY_ROOT_DIR}/lib/boost/lib
+            #${SMARTBODY_ROOT_DIR}/lib/bonebus/lib
+            #${SMARTBODY_ROOT_DIR}/lib/wsp/wsp/lib
+            #${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/build/win32/Debug
+            #${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/build/win32/Release
+            #${SMARTBODY_ROOT_DIR}/lib/vhmsg/vhmsg-c/lib64
+            #${SMARTBODY_ROOT_DIR}/lib/activemq/activemq-cpp/vs2010-build/DebugDLL64
+            #${SMARTBODY_ROOT_DIR}/lib/activemq/activemq-cpp/vs2010-build/ReleaseDLL
+            #${SMARTBODY_ROOT_DIR}/lib/xerces-c/lib
+			#/usr/local_rwth/sw/boost/1_53_0/gcc_4.4.6-openmpi_1.6.4/lib/
+			#/usr/local_rwth/sw/python/2.7.5/x86_64/lib/
+            #${SMARTBODY_ROOT_DIR}/core/smartbody/Python27/libs
         )
         else(CMAKE_SIZEOF_VOID_P EQUAL 8)
             set( SMARTBODY_LIBRARY_DIRS
-            /usr/local_rwth/sw/boost/1_53_0/gcc_4.4.6-openmpi_1.6.4/lib/
-		/usr/local_rwth/sw/python/2.7.5/x86_64/lib/
-		${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/lib
+			${SMARTBODY_ROOT_DIR}/bin    #not sure if necessary but this way it is added to runtime_path
+			${SMARTBODY_ROOT_DIR}/lib
+			${SMARTBODY_ROOT_DIR}/python27/libs
+            #/usr/local_rwth/sw/boost/1_53_0/gcc_4.4.6-openmpi_1.6.4/lib/
+			#/usr/local_rwth/sw/python/2.7.5/x86_64/lib/
+			#${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/lib
             #${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/src/external/glew
-            ${SMARTBODY_ROOT_DIR}/lib/vhcl/lib
-            ${SMARTBODY_ROOT_DIR}/lib/boost/lib
-            ${SMARTBODY_ROOT_DIR}/lib/bonebus/lib
-            ${SMARTBODY_ROOT_DIR}/lib/wsp/wsp/lib
-            ${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/build/win32/Debug
-            ${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/build/win32/Release
-            ${SMARTBODY_ROOT_DIR}/lib/vhmsg/vhmsg-c/lib64
-            ${SMARTBODY_ROOT_DIR}/lib/activemq/activemq-cpp/vs2010-build/DebugDLL
-            ${SMARTBODY_ROOT_DIR}/lib/activemq/activemq-cpp/vs2010-build/ReleaseDLL
-            ${SMARTBODY_ROOT_DIR}/lib/xerces-c/lib
-            ${SMARTBODY_ROOT_DIR}/lib/xerces-c/bin
-            ${SMARTBODY_ROOT_DIR}/lib/vhcl/libsndfile/bin/
-            ${SMARTBODY_ROOT_DIR}/lib/activemq/apr/apr/lib/
-            ${SMARTBODY_ROOT_DIR}/lib/activemq/apr/apr-util/lib/
-            ${SMARTBODY_ROOT_DIR}/lib/activemq/apr/apr-iconv/lib/
-            ${SMARTBODY_ROOT_DIR}/core/smartbody/Python27/libs/
-            ${SMARTBODY_ROOT_DIR}/lib/vhcl/openal/libs/Win32/ 
-            ${SMARTBODY_ROOT_DIR}/lib/vhcl/libsndfile/lib/  
-            ${SMARTBODY_ROOT_DIR}/lib/vhcl/libsndfile/bin/
-            ${SMARTBODY_ROOT_DIR}/lib/pthreads/lib/
+            #${SMARTBODY_ROOT_DIR}/lib/vhcl/lib
+            #${SMARTBODY_ROOT_DIR}/lib/boost/lib
+            #${SMARTBODY_ROOT_DIR}/lib/bonebus/lib
+            #${SMARTBODY_ROOT_DIR}/lib/wsp/wsp/lib
+            #${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/build/win32/Debug
+            #${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/build/win32/Release
+            #${SMARTBODY_ROOT_DIR}/lib/vhmsg/vhmsg-c/lib64
+            #${SMARTBODY_ROOT_DIR}/lib/activemq/activemq-cpp/vs2010-build/DebugDLL
+            #${SMARTBODY_ROOT_DIR}/lib/activemq/activemq-cpp/vs2010-build/ReleaseDLL
+            #${SMARTBODY_ROOT_DIR}/lib/xerces-c/lib
+            #${SMARTBODY_ROOT_DIR}/lib/xerces-c/bin
+            #${SMARTBODY_ROOT_DIR}/lib/vhcl/libsndfile/bin/
+            #${SMARTBODY_ROOT_DIR}/lib/activemq/apr/apr/lib/
+            #${SMARTBODY_ROOT_DIR}/lib/activemq/apr/apr-util/lib/
+            #${SMARTBODY_ROOT_DIR}/lib/activemq/apr/apr-iconv/lib/           
+            #${SMARTBODY_ROOT_DIR}/lib/vhcl/openal/libs/Win32/ 
+            #${SMARTBODY_ROOT_DIR}/lib/vhcl/libsndfile/lib/  
+            #${SMARTBODY_ROOT_DIR}/lib/vhcl/libsndfile/bin/
+            #${SMARTBODY_ROOT_DIR}/lib/pthreads/lib/
             )
         endif(CMAKE_SIZEOF_VOID_P EQUAL 8)
         
