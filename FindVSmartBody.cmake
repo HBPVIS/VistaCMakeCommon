009a50106bf15938be9b65a22d49d2d5f2c50b64
ah548455 <ah548455@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2014-01-20 13:33:34 +0000

    chg some linux specific stuff changed in the SDK
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@40131 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVSmartBody.cmake
+++ FindVSmartBody.cmake
@@ -10,20 +10,11 @@ if( NOT VSMARTBODY_FOUND )
 			${SMARTBODY_ROOT_DIR}/src/SmartBody
 			${SMARTBODY_ROOT_DIR}/include
 			${SMARTBODY_ROOT_DIR}/include/vhcl
-			${SMARTBODY_ROOT_DIR}/include/steersuite			
+			${SMARTBODY_ROOT_DIR}/include/steersuite
+            ${SMARTBODY_ROOT_DIR}/include/steersuite/external
 			${SMARTBODY_ROOT_DIR}/python27/include
 			${SMARTBODY_ROOT_DIR}/include/bonebus
-			${SMARTBODY_ROOT_DIR}/include/wsp
-            #${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/src/ 
-            #${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody-dll/include 
-            #${SMARTBODY_ROOT_DIR}/lib/vhcl/include
-            #${SMARTBODY_ROOT_DIR}/lib/boost/
-            #${SMARTBODY_ROOT_DIR}/lib/bonebus/include
-            #${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/steerlib/include
-            #${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/external/
-            #${SMARTBODY_ROOT_DIR}/lib/wsp/wsp/include/
-            #${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/src/external/glew/
-            
+			${SMARTBODY_ROOT_DIR}/include/wsp                      
         )
         
         if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
@@ -37,68 +28,31 @@ if( NOT VSMARTBODY_FOUND )
         endif()
         
         if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-            set( SMARTBODY_LIBRARY_DIRS 
-            #${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/lib
-            #${SMARTBODY_ROOT_DIR}/lib/vhcl/lib64
-            #${SMARTBODY_ROOT_DIR}/lib/boost/lib
-            #${SMARTBODY_ROOT_DIR}/lib/bonebus/lib
-            #${SMARTBODY_ROOT_DIR}/lib/wsp/wsp/lib
-            #${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/build/win32/Debug
-            #${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/build/win32/Release
-            #${SMARTBODY_ROOT_DIR}/lib/vhmsg/vhmsg-c/lib64
-            #${SMARTBODY_ROOT_DIR}/lib/activemq/activemq-cpp/vs2010-build/DebugDLL64
-            #${SMARTBODY_ROOT_DIR}/lib/activemq/activemq-cpp/vs2010-build/ReleaseDLL
-            #${SMARTBODY_ROOT_DIR}/lib/xerces-c/lib
-			#/usr/local_rwth/sw/boost/1_53_0/gcc_4.4.6-openmpi_1.6.4/lib/
-			#/usr/local_rwth/sw/python/2.7.5/x86_64/lib/
-            #${SMARTBODY_ROOT_DIR}/core/smartbody/Python27/libs
+            set( SMARTBODY_LIBRARY_DIRS            
+            ${SMARTBODY_ROOT_DIR}/lib
+            ${SMARTBODY_ROOT_DIR}/python27/libs
+            ${SMARTBODY_ROOT_DIR}/3rdParty/lib
         )
         else(CMAKE_SIZEOF_VOID_P EQUAL 8)
             set( SMARTBODY_LIBRARY_DIRS
 			${SMARTBODY_ROOT_DIR}/bin    #not sure if necessary but this way it is added to runtime_path
 			${SMARTBODY_ROOT_DIR}/lib
-			${SMARTBODY_ROOT_DIR}/python27/libs
-            #/usr/local_rwth/sw/boost/1_53_0/gcc_4.4.6-openmpi_1.6.4/lib/
-			#/usr/local_rwth/sw/python/2.7.5/x86_64/lib/
-			#${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/lib
-            #${SMARTBODY_ROOT_DIR}/core/smartbody/SmartBody/src/external/glew
-            #${SMARTBODY_ROOT_DIR}/lib/vhcl/lib
-            #${SMARTBODY_ROOT_DIR}/lib/boost/lib
-            #${SMARTBODY_ROOT_DIR}/lib/bonebus/lib
-            #${SMARTBODY_ROOT_DIR}/lib/wsp/wsp/lib
-            #${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/build/win32/Debug
-            #${SMARTBODY_ROOT_DIR}/core/smartbody/steersuite-1.3/build/win32/Release
-            #${SMARTBODY_ROOT_DIR}/lib/vhmsg/vhmsg-c/lib64
-            #${SMARTBODY_ROOT_DIR}/lib/activemq/activemq-cpp/vs2010-build/DebugDLL
-            #${SMARTBODY_ROOT_DIR}/lib/activemq/activemq-cpp/vs2010-build/ReleaseDLL
-            #${SMARTBODY_ROOT_DIR}/lib/xerces-c/lib
-            #${SMARTBODY_ROOT_DIR}/lib/xerces-c/bin
-            #${SMARTBODY_ROOT_DIR}/lib/vhcl/libsndfile/bin/
-            #${SMARTBODY_ROOT_DIR}/lib/activemq/apr/apr/lib/
-            #${SMARTBODY_ROOT_DIR}/lib/activemq/apr/apr-util/lib/
-            #${SMARTBODY_ROOT_DIR}/lib/activemq/apr/apr-iconv/lib/           
-            #${SMARTBODY_ROOT_DIR}/lib/vhcl/openal/libs/Win32/ 
-            #${SMARTBODY_ROOT_DIR}/lib/vhcl/libsndfile/lib/  
-            #${SMARTBODY_ROOT_DIR}/lib/vhcl/libsndfile/bin/
-            #${SMARTBODY_ROOT_DIR}/lib/pthreads/lib/
+			${SMARTBODY_ROOT_DIR}/python27/libs            
             )
         endif(CMAKE_SIZEOF_VOID_P EQUAL 8)
+        
+        if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")            
+            else() # Linux   
+            list(APPEND SMARTBODY_LIBRARY_DIRS
+                /usr/local_rwth/sw/boost/1_53_0/gcc-openmpi/lib
+                /usr/local_rwth/sw/python/2.7.5/x86_64/lib
+            )    
+        endif()
+        
         
         set( SMARTBODY_LIBRARIES            
             optimized SmartBody
             debug SmartBody_d
-            #linux ? xerces-c
-            #optimized bonebus
-            #debug bonebus_d
-            
-            #optimized vhmsg
-            #optimized wsp
-            #debug activemq-cppd
-            #optimized libactivemq-cpp.so.14
-            
-            
-            # linux ?boost_filesystem    
-
         )
 
     if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
@@ -112,25 +66,17 @@ if( NOT VSMARTBODY_FOUND )
         boost_system-vc100-mt-1_51.lib #windows ?        
         optimized steerlib
         debug steerlibd
-        
-        #boost_filesystem-vc100-mt-gd-1_51.lib
-#boost_system-vc100-mt-gd-1_51.lib
-#boost_regex-vc100-mt-gd-1_51.lib
-#boost_python-vc100-mt-gd-1_51.lib
-#boost_serialization-vc100-mt-gd-1_51.lib
-     
-  
     )
     else() # Linux   
     list(APPEND SMARTBODY_LIBRARIES
         xerces-c
         boost_filesystem
         boost_system
-	boost_regex
-	boost_python
+        boost_regex
+        boost_python
         pprAI
-	steerlib
-	python2.7
+        steerlib
+        python2.7
     )    
     endif()
         
