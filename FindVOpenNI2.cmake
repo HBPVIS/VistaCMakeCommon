b9d6a3ac12ce5e76c5d7e4b482c9190a85d4d3f2
ps790413 <ps790413@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2014-01-21 13:38:35 +0000

    ADD: preliminary cmake file for OpenNI2
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@40155 33c38b33-09d2-4561-8d4d-1bfa2f58f885

---
+++ FindVOpenNI2.cmake
@@ -0,0 +1,15 @@
+# $Id: FindVopenni.cmake 21495 2011-05-25 07:52:18Z dr165799 $
+
+include( FindPackageHandleStandardArgs )
+include( VistaFindUtils )
+
+if( NOT VOPENNI2_FOUND )
+	vista_find_package_root( OpenNI2 Include/OpenNI.h )
+
+	if( OPENNI2_ROOT_DIR )
+		set( OPENNI2_INCLUDE_DIRS "${OPENNI2_ROOT_DIR}/Include" )
+		set( OPENNI2_LIBRARY_DIRS "${OPENNI2_ROOT_DIR}/Bin/x64-Debug" )
+	endif( OPENNI2_ROOT_DIR )
+endif( NOT VOPENNI2_FOUND )
+
+find_package_handle_standard_args( VOpenNI2 "OpenNI could not be found" OPENNI2_ROOT_DIR )
