58352ee00f73be63f8515bea1128fb1736635fc9
ps790413 <ps790413@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2014-01-24 15:47:34 +0000

    ADD: pathes to search and include dirs for freetype-2.5 deployment structure
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@40217 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVFreeType.cmake
+++ FindVFreeType.cmake
@@ -6,6 +6,9 @@ include( VistaFindUtils )
 if( NOT VFREETYPE_FOUND )
 
 	vista_find_package_root( FreeType include/freetype/freetype.h )
+	if( NOT FREETYPE_ROOT_DIR )
+		vista_find_package_root( FreeType include/freetype2/freetype.h )
+	endif( NOT FREETYPE_ROOT_DIR )
 
 	if( FREETYPE_ROOT_DIR )
 		find_library( FREETYPE_LIBRARIES NAMES freetype FreeType
@@ -13,7 +16,8 @@ if( NOT VFREETYPE_FOUND )
 					CACHE "FREETYPE library" )
 		mark_as_advanced( FREETYPE_LIBRARIES )
 
-		set( FREETYPE_INCLUDE_DIRS ${FREETYPE_ROOT_DIR}/include )
+		set( FREETYPE_INCLUDE_DIRS ${FREETYPE_ROOT_DIR}/include
+								   ${FREETYPE_ROOT_DIR}/include/freetype2 )
 		set( FREETYPE_LIBRARY_DIRS ${FREETYPE_ROOT_DIR}/lib )
 		get_filename_component( FREETYPE_LIBRARY_DIRS ${FREETYPE_LIBRARIES} PATH )
     else ( FREETYPE_ROOT_DIR )
