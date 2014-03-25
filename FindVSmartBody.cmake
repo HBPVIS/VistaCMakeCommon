0e83bc5ad53d6a4c7ebb01d89ed4c7bc27e3e2af
ah548455 <ah548455@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2014-03-25 10:08:10 +0000

    enabled version checking by not capitalizing
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@41340 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVSmartBody.cmake
+++ FindVSmartBody.cmake
@@ -4,7 +4,8 @@ include( FindPackageHandleStandardArgs )
 include( VistaFindUtils )
 
 if( NOT VSMARTBODY_FOUND )
-	vista_find_package_root( SMARTBODY /src/SmartBody/sb/SBTypes.h NAMES SmartBodySDK smartbody SmartBody )
+	vista_find_package_root( SmartBody /src/SmartBody/sb/SBTypes.h NAMES SmartBodySDK smartbody SmartBody )
+    
 	if( SMARTBODY_ROOT_DIR )
 		set( SMARTBODY_INCLUDE_DIRS 
 			${SMARTBODY_ROOT_DIR}/src/SmartBody
@@ -29,20 +30,20 @@ if( NOT VSMARTBODY_FOUND )
         
         if(CMAKE_SIZEOF_VOID_P EQUAL 8)
             set( SMARTBODY_LIBRARY_DIRS            
-            ${SMARTBODY_ROOT_DIR}/lib
-            ${SMARTBODY_ROOT_DIR}/python27/libs
-            ${SMARTBODY_ROOT_DIR}/3rdParty/lib
-        )
+                ${SMARTBODY_ROOT_DIR}/lib
+                ${SMARTBODY_ROOT_DIR}/python27/libs
+                ${SMARTBODY_ROOT_DIR}/3rdParty/lib
+               )
         else(CMAKE_SIZEOF_VOID_P EQUAL 8)
             set( SMARTBODY_LIBRARY_DIRS
-			${SMARTBODY_ROOT_DIR}/bin    #not sure if necessary but this way it is added to runtime_path
-			${SMARTBODY_ROOT_DIR}/lib
-			${SMARTBODY_ROOT_DIR}/python27/libs            
-            )
+                ${SMARTBODY_ROOT_DIR}/bin    #not sure if necessary but this way it is added to runtime_path
+                ${SMARTBODY_ROOT_DIR}/lib
+                ${SMARTBODY_ROOT_DIR}/python27/libs            
+               )
         endif(CMAKE_SIZEOF_VOID_P EQUAL 8)
         
         if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")            
-            else() # Linux   
+        else() # Linux   
             list(APPEND SMARTBODY_LIBRARY_DIRS
                 /usr/local_rwth/sw/boost/1_53_0/gcc-openmpi/lib
                 /usr/local_rwth/sw/python/2.7.5/x86_64/lib
@@ -55,34 +56,31 @@ if( NOT VSMARTBODY_FOUND )
             debug SmartBody_d
         )
 
-    if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-        list(APPEND SMARTBODY_LIBRARIES
-        optimized xerces-c_3.lib
-        debug xerces-c_3D.lib
-        glew32 #windows
-        optimized vhcl #windows
-        debug vhcl_d #windows        
-        boost_filesystem-vc100-mt-1_51.lib #windows ?
-        boost_system-vc100-mt-1_51.lib #windows ?        
-        optimized steerlib
-        debug steerlibd
-    )
-    else() # Linux   
-    list(APPEND SMARTBODY_LIBRARIES
-        xerces-c
-        boost_filesystem
-        boost_system
-        boost_regex
-        boost_python
-        pprAI
-        steerlib
-        python2.7
-    )    
+        if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+            list(APPEND SMARTBODY_LIBRARIES
+                optimized xerces-c_3.lib
+                debug xerces-c_3D.lib
+                glew32 #windows
+                optimized vhcl #windows
+                debug vhcl_d #windows        
+                boost_filesystem-vc100-mt-1_51.lib #windows ?
+                boost_system-vc100-mt-1_51.lib #windows ?        
+                optimized steerlib
+                debug steerlibd
+            )
+        else() # Linux   
+            list(APPEND SMARTBODY_LIBRARIES
+                xerces-c
+                boost_filesystem
+                boost_system
+                boost_regex
+                boost_python
+                pprAI
+                steerlib
+                python2.7
+            )    
+        endif()
     endif()
-        
-	else( SMARTBODY_ROOT_DIR )
-		message( WARNING "vista_find_package_root - File named /src/SmartBody/sb/SBTypes.h not found" )	
-	endif( SMARTBODY_ROOT_DIR )
 endif( NOT VSMARTBODY_FOUND )
 
-find_package_handle_standard_args( VSMARTBODY "SmartBody could not be found" SMARTBODY_ROOT_DIR )
+find_package_handle_standard_args( VSmartBody "SmartBody could not be found" SMARTBODY_ROOT_DIR )
