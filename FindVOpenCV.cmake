4ff6b562afe6f45f8d43b14677d603d6d851def7
sf458740 <sf458740@33c38b33-09d2-4561-8d4d-1bfa2f58f885>
2014-05-07 09:11:19 +0000

    CHG: FindVOpenCV.cmake works with changed structure in OpenCV 2.4.9
    git-svn-id: https://svn.rwth-aachen.de/repos/vrgroup-svn/projects/VistaCMakeCommon/trunk@41836 33c38b33-09d2-4561-8d4d-1bfa2f58f885

--- FindVOpenCV.cmake
+++ FindVOpenCV.cmake
@@ -1,5 +1,6 @@
 # $Id: FindVOPENCV.cmake 21495 2011-05-25 07:52:18Z dr165799 $
 
+
 include( FindPackageHandleStandardArgs )
 include( VistaFindUtils )
 
@@ -19,14 +20,18 @@ if( NOT VOPENCV_FOUND )
 	set( CMAKE_PREFIX_PATH "${OPENCV_ROOT_DIR}" ${CMAKE_PREFIX_PATH} )
 	vista_find_original_package( VOpenCV )
 	# an OpenCVConfig.cmake has been found and loaded
+	
+	unset ( OPENCV_FOUND CACHE )
+	
 	if( OpenCV_FOUND )
 		set( OPENCV_LIBRARIES "${OpenCV_LIBS}" )
-		set( OPENCV_LIBRARY_DIRS "${OpenCV_LIB_DIR}" "${OpenCV_DIR}/bin" )
+		set( OPENCV_LIBRARY_DIRS "${OpenCV_LIB_DIR}" "${OpenCV_LIB_DIR}/../bin" )
 		set( OPENCV_INCLUDE_DIRS "${OpenCV_INCLUDE_DIRS}" )
 		set( OPENCV_VERSION "${OpenCV_VERSION}" )
 	endif()
 	
 endif( NOT VOPENCV_FOUND )
+
 
 find_package_handle_standard_args( VOpenCV "OpenCV could not be found" OPENCV_LIBRARIES )
 
